1182658778e1915c5210ba4d2d2b6fa58ac8f53b
atb033 <ashwinbose123@gmail.com>
2018-12-31 14:56:10 +0100

    multiple views

--- sfm_main.m
+++ sfm_main.m
@@ -19,7 +19,7 @@ end
 %% Load camera parameters
 calib = loadCalibrationCamToCam(fullfile(calib_dir,'calib_cam_to_cam.txt'));
 % Create cameraParameters object from calib
-cameraParam = cameraParameters('IntrinsicMatrix',calib.K{cam});
+cameraParams = cameraParameters('IntrinsicMatrix',calib.K{cam});
 
 I = images{1};
 
@@ -43,7 +43,62 @@ vSet = addView(vSet, viewId, 'Points', prevPoints, 'Orientation', ...
 
 
 %% Add the Rest of the Views
+for i = 2:numel(images)
+    % Detect, extract and match features.
+    currPoints   = detectSURFFeatures(I, 'NumOctaves', 8, 'ROI', roi);
+    currFeatures = extractFeatures(I, currPoints, 'Upright', true);    
+    indexPairs = matchFeatures(prevFeatures, currFeatures, ...
+        'MaxRatio', .7, 'Unique',  true);
+    
+    % Select matched points.
+    matchedPoints1 = prevPoints(indexPairs(:, 1));
+    matchedPoints2 = currPoints(indexPairs(:, 2));
+    
+    % Estimate the camera pose of current view relative to the previous view.
+    % The pose is computed up to scale, meaning that the distance between
+    % the cameras in the previous view and the current view is set to 1.
+    % This will be corrected by the bundle adjustment.
+    [relativeOrient, relativeLoc, inlierIdx] = helperEstimateRelativePose(...
+        matchedPoints1, matchedPoints2, cameraParams);
+    
+    % Add the current view to the view set.
+    vSet = addView(vSet, i, 'Points', currPoints);
+    
+    % Store the point matches between the previous and the current views.
+    vSet = addConnection(vSet, i-1, i, 'Matches', indexPairs(inlierIdx,:));
+    
+    % Get the table containing the previous camera pose.
+    prevPose = poses(vSet, i-1);
+    prevOrientation = prevPose.Orientation{1};
+    prevLocation    = prevPose.Location{1};
+        
+    % Compute the current camera pose in the global coordinate system 
+    % relative to the first view.
+    orientation = relativeOrient * prevOrientation;
+    location    = prevLocation + relativeLoc * prevOrientation;
+    vSet = updateView(vSet, i, 'Orientation', orientation, ...
+        'Location', location);
+    
+    % Find point tracks across all views.
+    tracks = findTracks(vSet);
 
+    % Get the table containing camera poses for all views.
+    camPoses = poses(vSet);
+
+    % Triangulate initial locations for the 3-D world points.
+    xyzPoints = triangulateMultiview(tracks, camPoses, cameraParams);
+    
+    % Refine the 3-D world points and camera poses.
+    [xyzPoints, camPoses, reprojectionErrors] = bundleAdjustment(xyzPoints, ...
+        tracks, camPoses, cameraParams, 'FixedViewId', 1, ...
+        'PointsUndistorted', true);
+
+    % Store the refined camera poses.
+    vSet = updateView(vSet, camPoses);
+
+    prevFeatures = currFeatures;
+    prevPoints   = currPoints;  
+end
 % Display Camera Poses
 
 % Compute Dense Reconstruction
